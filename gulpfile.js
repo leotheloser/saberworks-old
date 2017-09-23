14632f3845b3822ff808ad14722f21f9cff0febb
Samrura <samsixpak@hotmail.com>
2017-09-23 16:16:48 +1200

    csssss

--- gulpfile.js
+++ gulpfile.js
@@ -26,7 +26,7 @@ gulp.task('browserSync', function() {
 gulp.task('sass', function() {
   return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
     .pipe(sass().on('error', sass.logError)) // Passes it through a gulp-sass, log errors to console
-    .pipe(gulp.dest('app/css')) // Outputs it in the css folder
+    .pipe(gulp.dest('app/')) // Outputs it in the css folder
     .pipe(browserSync.reload({ // Reloading with Browser Sync
       stream: true
     }));
